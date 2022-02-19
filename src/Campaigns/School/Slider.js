a044fcd18c1f2cfda111568f9d4e0594f218f493
Franco Jara <jarafranco122@gmail.com>
2022-02-19 13:21:16 -0300

    adding dynamic content

--- src/Campaigns/School/Slider.js
+++ src/Campaigns/School/Slider.js
@@ -1,59 +1,75 @@
 import styled from "@emotion/styled";
 import React from "react";
 import SwipeableViews from "react-swipeable-views";
+import Button from "@mui/material/Button";
 
-const styles = {
-	slide: {
-		padding: 15,
-		minHeight: 100,
-		color: "#fff",
-	},
-	slide1: {
-		background: "#FEA900",
-	},
-	slide2: {
-		background: "#B3DC4A",
-	},
-	slide3: {
-		background: "#6AC0FF",
-	},
-	img: {
-		maxWidth: "50%",
-	},
-};
+const slidesContent = [{}];
 
 const SliderContainer = styled.div`
-  display: flex;
-  padding: 15%;
-	min-height: 100;
-	color: "#fff";
-`
+	display: flex;
+	/* min-height: 250px;
+	max-height: 330px; */
+	height: 250px;
+	color: #fff;
+	/* background: #FEA900; */
+	background: ${(props) => (props.background ? props.background : '#FEA900')};
+	/* max-height: 400; */
+`;
 
-const SliderOne = () => {
+const TextContainer = styled.div`
+	width: 100%;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	flex-direction: column;
+`;
+
+const SliderImage = styled.img`
+	object-fit: cover;
+	max-width: 40%;
+	max-height: 100%;
+`;
+
+const Slide = ({
+	description = "slide description",
+	title = "title",
+	buttonText = "ver más",
+	img = null,
+	backgroundColor = null
+}) => {
 	return (
-		<SliderContainer >
-			<p>slide n°1</p>
-			<img
-				style={Object.assign({}, styles.img)}
-				src="https://www.rover.com/blog/wp-content/uploads/2019/04/cute-big-eyes-960x540-1.jpg"
+		<SliderContainer background={backgroundColor} >
+			<SliderImage
+				// style={Object.assign({}, styles.img)}
+				src={img}
 				alt=""
 			/>
+			<TextContainer>
+				<h1>{title}</h1>
+				<p>{description}</p>
+				<Button variant="contained">{buttonText}</Button>
+			</TextContainer>
 		</SliderContainer>
 	);
 };
 
-const Slider = () => {
+const Slider = ({
+	slidersData = [
+		{ id: 1, name: "test2", description: "test description2", backgroundColor: '#12579d', image: 'https://www.rover.com/blog/wp-content/uploads/2019/04/cute-big-eyes-960x540-1.jpg' },
+		{ id: 2, name: "test3", description: "test description3", backgroundColor: '#12579d', image: 'https://p4.wallpaperbetter.com/wallpaper/181/205/221/cats-love-wallpaper-preview.jpg' },
+		{ id: 3, name: "test3", description: "test description3", image: 'http://cdn26.us1.fansshare.com/photo/animalwallpapers/baby-animals-kittens-kitten-cats-cute-cat-picture-photo-animal-wallpapers-1049853647.jpg' },
+		// { name: "test", description: "test description" },
+		// { name: "test2", description: "test description2" },
+		// { name: "test2", description: "test description2" },
+	],
+}) => {
 	return (
 		<SwipeableViews enableMouseEvents>
-			<div style={Object.assign({}, styles.slide, styles.slide1)}>
-				<SliderOne />
-			</div>
-			<div style={Object.assign({}, styles.slide, styles.slide2)}>
-				slide n°2
-			</div>
-			<div style={Object.assign({}, styles.slide, styles.slide3)}>
-				slide n°3
-			</div>
+			{slidersData.map((slideData) => {
+				return (
+					<Slide key={slideData.id} title={slideData.name} description={slideData.description} backgroundColor={slideData.backgroundColor} img={slideData.image} />
+				);
+			})}
 		</SwipeableViews>
 	);
 };
