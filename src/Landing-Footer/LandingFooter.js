27fe0fc833d9d877e69d6b358b9b0a13358d0b1f
Yesica Oviedo <oviedoyesica52@gmail.com>
2022-02-20 12:03:59 -0300

    change style and add links

--- src/Landing-Footer/LandingFooter.js
+++ src/Landing-Footer/LandingFooter.js
@@ -1,46 +1,69 @@
 import React from 'react';
 import { styled } from '@mui/material/styles';
 import FacebookIcon from '@mui/icons-material/Facebook';
-import { blue } from '@mui/material/colors';
 
 
 const Root = styled('div')(({ theme }) => ({
     [theme.breakpoints.down('md')]: {
       display: 'block',
-
+      position:'fixed',
+      left:0,
+      bottom:'0px',
+      zIndex:'99',
+      border:'0',
     },
     [theme.breakpoints.up('md')]: {
         display:'flex',
+        position:'fixed',
+        left:0,
+        bottom:'0px',
+        zIndex:'99',
+        border:'0',
     },
   }));
 
   const Images = styled('img')(({theme}) => ({
     [theme.breakpoints.down('md')]: {
         maxWidth: '20%',
-        paddingBotom: '50%',
+        paddingBottom: '10%',
+        display: 'none',
       },
     [theme.breakpoints.up('md')]:{
-        maxWidth: '30%',
-
+        maxWidth: '20%',
     }
   }));
 
+  const Logo = styled('img')(({theme}) => ({
+    [theme.breakpoints.down('md')]: {
+        maxWidth: '30%',
+        margin:'0 auto'
+      },
+    [theme.breakpoints.up('md')]:{
+        maxWidth: '40%',
+    }
+  }));
+
+
 const LandingFooter = () => {
-
-
     return (
         <Root>
             <div>
-                <Images src="/Images/LOGO-SOMOS MAS.png"/>
+                <Logo src="/Images/LOGO-SOMOS MAS.png"/>
             </div>
             <div>
-                <Images src="/Images/Logotipo campaña juguetes.png"/>
+                <a href="/toys-campaign">
+                    <Images src="/Images/Logotipo campaña juguetes.png"/>
+                </a>
             </div>
             <div>
-                <Images src="/Images/Logotipo campaña materiales escolares.png"/>
+                <a href="/school-campaign">
+                    <Images src="/Images/Logotipo campaña materiales escolares.png"/>
+                </a>
             </div>
             <div>
-                <FacebookIcon style={{fontSize: '50px'}}/>
+                <a href="https://es-la.facebook.com" text-decoration='none'>
+                    <FacebookIcon style={{fontSize: '50px', color:'secondary' ,color: '#3b5998'}}/>
+                </a>
             </div>
         </Root>
     );
