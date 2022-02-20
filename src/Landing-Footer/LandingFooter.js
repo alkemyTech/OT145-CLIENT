9121e7982f529f26da5f24d66fa02d1b8b5f8335
Yesica Oviedo <oviedoyesica52@gmail.com>
2022-02-20 14:15:01 -0300

    styles for different resolutions

--- src/Landing-Footer/LandingFooter.js
+++ src/Landing-Footer/LandingFooter.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import { styled } from '@mui/material/styles';
 import FacebookIcon from '@mui/icons-material/Facebook';
+import { Typography } from '@mui/material';
+import InstagramIcon from '@mui/icons-material/Instagram';
+
 
 
 const Root = styled('div')(({ theme }) => ({
     [theme.breakpoints.down('md')]: {
-      display: 'block',
-      position:'fixed',
-      left:0,
-      bottom:'0px',
-      zIndex:'99',
-      border:'0',
+        display:'flex',
+        position:'fixed',
+        left:0,
+        bottom:'0px',
+        zIndex:'99',
+        border:'0',
     },
     [theme.breakpoints.up('md')]: {
         display:'flex',
@@ -22,26 +25,48 @@ const Root = styled('div')(({ theme }) => ({
     },
   }));
 
-  const Images = styled('img')(({theme}) => ({
-    [theme.breakpoints.down('md')]: {
-        maxWidth: '20%',
-        paddingBottom: '10%',
-        display: 'none',
-      },
-    [theme.breakpoints.up('md')]:{
-        maxWidth: '20%',
-    }
-  }));
-
   const Logo = styled('img')(({theme}) => ({
     [theme.breakpoints.down('md')]: {
-        maxWidth: '30%',
-        margin:'0 auto'
+        maxWidth: '40%',
       },
     [theme.breakpoints.up('md')]:{
         maxWidth: '40%',
     }
   }));
+
+  const AHome = styled('a')(({theme}) => ({
+    [theme.breakpoints.down('sm')]: {
+        display: 'none',
+      },
+    [theme.breakpoints.up('sm')]:{
+        maxWidth: '40%',
+    }
+  }));
+
+  const ARedes = styled('a')(({theme}) => ({
+    [theme.breakpoints.down('md')]: {
+        display: 'none',
+      },
+    [theme.breakpoints.up('md')]:{
+        maxWidth: '40%',
+    }
+  }));
+
+  const ACampaings = styled('a')(({theme}) => ({
+    [theme.breakpoints.down('lg')]: {
+        display: 'none',
+      },
+    [theme.breakpoints.up('lg')]:{
+        maxWidth: '40%',
+    }
+
+
+  }));
+
+
+
+
+
 
 
 const LandingFooter = () => {
@@ -50,21 +75,33 @@ const LandingFooter = () => {
             <div>
                 <Logo src="/Images/LOGO-SOMOS MAS.png"/>
             </div>
-            <div>
-                <a href="/toys-campaign">
-                    <Images src="/Images/Logotipo campaña juguetes.png"/>
+            <AHome href="/Home" >
+                <Typography style={{fontSize: '15px',paddingTop:28, paddingRight:20}}>Somos más</Typography>
+            </AHome>
+            <div style={{display:'flex'}}>
+                <a href="https://es-la.facebook.com">
+                    <FacebookIcon style={{fontSize: '40px',paddingTop:20,color: '#3b5998'}}/>
                 </a>
+                <ARedes href="https://es-la.facebook.com" >
+                    <Typography style={{fontSize: '15px',paddingRight:20,paddingTop:28}}>Facebook</Typography>
+                </ARedes>
+                
             </div>
-            <div>
-                <a href="/school-campaign">
-                    <Images src="/Images/Logotipo campaña materiales escolares.png"/>
+            <div style={{display:'flex'}}>
+                <a href="https://www.instagram.com">
+                    <InstagramIcon style={{fontSize: '40px',paddingTop:20,color: '#bc2a8d'}}/>
                 </a>
+                <ARedes href="https://www.instagram.com" >
+                    <Typography style={{fontSize: '15px',paddingTop:28}}>Instagram</Typography>
+                </ARedes>
             </div>
-            <div>
-                <a href="https://es-la.facebook.com" text-decoration='none'>
-                    <FacebookIcon style={{fontSize: '50px', color:'secondary' ,color: '#3b5998'}}/>
-                </a>
-            </div>
+            <ACampaings href="/toys-campaign">
+                <Typography  style={{fontSize: '15px',paddingTop:28, paddingRight:20,paddingLeft:20}}> Toys Campaign </Typography>
+            </ACampaings>
+            <ACampaings href="/school-campaign">
+                <Typography style={{fontSize: '15px',paddingTop:28, paddingRight:20,paddingLeft:20}}> School Campaign </Typography> 
+            </ACampaings>
+
         </Root>
     );
 }
