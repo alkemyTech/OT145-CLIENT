7bc4d092dd28fc52c630c6b1ba38c17d6791ddd6
Yesica Oviedo <oviedoyesica52@gmail.com>
2022-02-22 21:56:26 -0300

    corrijo cambios en la app

--- src/App.js
+++ src/App.js
@@ -1,20 +1,3 @@
-<<<<<<< HEAD
-import React from 'react';
-import {BrowserRouter, Route, Switch} from 'react-router-dom';
-import ActivitiesForm from './Components/Activities/ActivitiesForm';
-import CategoriesForm from './Components/Categories/CategoriesForm';
-import NewsForm from './Components/News/NewsForm';
-import SlidesForm from './Components/Slides/SlidesForm';
-import TestimonialForm from './Components/Testimonials/TestimonialsForm';
-import UserForm from './Components/Users/UsersForm';
-import SchoolCampaign from './Campaigns/School/SchoolCampaign';
-import ToysCampaign from './Campaigns/Toys/ToysCampaign';
-import MembersForm from './Components/Members/MembersForm';
-import ProjectsForm from './Components/Projects/ProjectsForm';
-import { theme } from './theme';
-import { ThemeProvider } from '@mui/material';
-
-=======
 import React from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import ActivitiesForm from './Components/Activities/ActivitiesForm'
@@ -29,7 +12,6 @@ import MembersForm from './Components/Members/MembersForm'
 import ProjectsForm from './Components/Projects/ProjectsForm'
 import { theme } from './theme'
 import { ThemeProvider } from '@mui/material'
->>>>>>> ccff57115218dc0c2b968248f06beec023c293d3
 
 function App() {
   return (
