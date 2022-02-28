d2c1d295ea1d08cc185d8161cfa0174f1ed5070e
Jesus Sivira Escobar <eduardosivira17@gmail.com>
2022-02-28 19:56:22 -0300

    Se actualizo el componente Alert y se registro en el readme

--- src/App.js
+++ src/App.js
@@ -13,23 +13,40 @@ import ProjectsForm from './Components/Projects/ProjectsForm'
 import { theme } from './theme'
 import { ThemeProvider } from '@mui/material'
 
+const ActivitiesForm = lazy(() => import('./Components/Activities/ActivitiesForm'));
+const CategoriesForm = lazy(() => import('./Components/Categories/CategoriesForm'));
+const NewsForm = lazy(() => import('./Components/News/NewsForm'));
+const SlidesForm = lazy(() => import('./Components/Slides/SlidesForm'));
+const TestimonialForm = lazy(() => import('./Components/Testimonials/TestimonialsForm'));
+const UserForm = lazy(() => import('./Components/Users/UsersForm'));
+const ToysCampaign = lazy(() => import('./Campaigns/Toys/ToysCampaign'));
+const SchoolCampaign = lazy(() => import('./Campaigns/School/SchoolCampaign'));
+const MembersForm = lazy(() => import('./Components/Members/MembersForm'));
+const ProjectsForm = lazy(() => import('./Components/Projects/ProjectsForm'));
+const News = lazy(() => import('./Components/News/News'));
+const NewsDetail =  lazy(() => import('./Components/News/Detail/NewsDetail'));
+
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <BrowserRouter>
-        <Switch>
-          {/* <Route path="/" exact component={} />           Esta ruta debe ser para el Home */}
-          <Route path="/create-activity" component={ActivitiesForm} />
-          <Route path="/create-category" component={CategoriesForm} />
-          <Route path="/create-news" component={NewsForm} />
-          <Route path="/backoffice/create-slide" component={SlidesForm} />
-          <Route path="/create-testimonials" component={TestimonialForm} />
-          <Route path="/create-user" component={UserForm} />
-          <Route path="/create-member" component={MembersForm} />
-          <Route path="/create-project" component={ProjectsForm} />
-          <Route path="/school-campaign" component={SchoolCampaign} />
-          <Route path="/toys-campaign" component={ToysCampaign} />
-        </Switch>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Switch>
+            {/* <Route path="/" exact component={} />           Esta ruta debe ser para el Home */}
+            <Route path="/create-activity" component={ActivitiesForm} />
+            <Route path="/create-category" component={CategoriesForm} />
+            <Route path="/create-news" component={NewsForm} />
+            <Route path="/backoffice/create-slide" component={SlidesForm} />
+            <Route path="/create-testimonials" component={TestimonialForm} />
+            <Route path="/create-user" component={UserForm} />
+            <Route path="/create-member" component={MembersForm} />
+            <Route path="/create-project" component={ProjectsForm} />
+            <Route path="/school-campaign" component={SchoolCampaign} />
+            <Route path="/toys-campaign" component={ToysCampaign} />
+            <Route exact path="/news" component={News}  />
+            <Route path="/news/:id" component={NewsDetail} />
+          </Switch>
+        </Suspense>
       </BrowserRouter>
     </ThemeProvider>
   )
