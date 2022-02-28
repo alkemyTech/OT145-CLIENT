ed9e34b5ff8d8d49d26dbe8c92ee0a74a9c84b9a
Yesica Oviedo <oviedoyesica52@gmail.com>
2022-02-28 16:04:21 -0300

    getService method is modified

--- src/Services/publicApiService.js
+++ src/Services/publicApiService.js
@@ -6,24 +6,16 @@ const config = {
     }
 }
 
-const Get = () => {
-    axios.get('https://jsonplaceholder.typicode.com/users', config)
-    .then(res => console.log(res))
-    .catch(err => console.log(err))
-}
-
-export default Get;
-
-
-export const getService = async (URL, Id) => {
+const getServicePublic = async (URL, Id) => {
     try{
         if(!Id){
-            const response = await axios.get(URL + "/users");
+            const response = await axios.get(URL);
             return response.data;
         }
-        const response = await axios.get(URL + "/users/" + Id);
+        const response = await axios.get(URL + Id);
         return response.data;
     }catch (error){
         console.error(error);
     }
 }
+export default getServicePublic;
