cc644861a8488ef75591c004e744241e07af86b2
andy <andycambra@gmail.com>
2022-02-28 18:38:45 -0300

    Se realizó método PATCH

--- src/Services/privateApiService.js
+++ src/Services/privateApiService.js
@@ -1,40 +1,34 @@
-import axios from 'axios';
+import axios from 'axios'
 
 const getAuthorizationHeader = () => {
-    const token = localStorage.getItem('token');
-    const auth = 'Portador:  ' + token;
-  
-    return token !== null ? auth : null;
-  };
+  const token = localStorage.getItem('token')
+  const auth = 'Portador:  ' + token
 
-const config = {
-    headers: {
-        Group: 145,               //Aqui va el ID del equipo!!
-        Authorization: getAuthorizationHeader()
-    }
+  return token !== null ? auth : null
 }
 
 const config = {
-    headers: {
-      Group: 145, 
-      Authorization: getAuthorizationHeader(),
-    },
-  };
+  headers: {
+    Group: 145,
+    Authorization: getAuthorizationHeader(),
+  },
+}
 
 const Get = () => {
-    axios.get('https://jsonplaceholder.typicode.com/users', config)
-    .then(res => console.log(res))
-    .catch(err => console.log(err))
+  axios
+    .get('https://jsonplaceholder.typicode.com/users', config)
+    .then((res) => console.log(res))
+    .catch((err) => console.log(err))
 }
 
 export const privatePUT = async (path, id, body) => {
-    try {
-      const response = await axios.put(`${path}/${id}`, body, config);
-  
-      return response.data;
-    } catch (error) {
-      console.error(error);
-    }
-  };
+  try {
+    const response = await axios.put(`${path}/${id}`, body, config)
+
+    return response.data
+  } catch (error) {
+    console.error(error)
+  }
+}
 
 export default Get
