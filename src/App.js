ec2cbdd1767d9855cc4709f7157aa634a2e1c5b2
Gabriel Barrientos <gabrielbarrientos729@gmail.com>
2022-03-01 12:51:59 -0300

    se agrego trancision a las rutas de app

--- src/App.js
+++ src/App.js
@@ -1,36 +1,36 @@
 import React, { Suspense, lazy } from 'react'
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import { BrowserRouter, Route } from 'react-router-dom'
 import { theme } from './theme'
 import { ThemeProvider } from '@mui/material'
+import { AnimatedSwitch } from 'react-router-transition'
 
-const ActivitiesForm = lazy(() =>
-  import('./Components/Activities/ActivitiesForm'),
-)
-const CategoriesForm = lazy(() =>
-  import('./Components/Categories/CategoriesForm'),
-)
+
+const ActivitiesForm = lazy(() => import('./Components/Activities/ActivitiesForm'))
+const CategoriesForm = lazy(() => import('./Components/Categories/CategoriesForm'))
 const NewsForm = lazy(() => import('./Components/News/NewsForm'))
 const SlidesForm = lazy(() => import('./Components/Slides/SlidesForm'))
-const TestimonialForm = lazy(() =>
-  import('./Components/Testimonials/TestimonialsForm'),
-)
+const TestimonialForm = lazy(() => import('./Components/Testimonials/TestimonialsForm'))
 const UserForm = lazy(() => import('./Components/Users/UsersForm'))
 const ToysCampaign = lazy(() => import('./Campaigns/Toys/ToysCampaign'))
 const SchoolCampaign = lazy(() => import('./Campaigns/School/SchoolCampaign'))
 const MembersForm = lazy(() => import('./Components/Members/MembersForm'))
 const ProjectsForm = lazy(() => import('./Components/Projects/ProjectsForm'))
-const Actividades = lazy(() =>
-  import('../src/Components/Activities/Actividades'),
-)
+const Actividades = lazy(() => import('../src/Components/Activities/Actividades'))
 const BackOficce = lazy(() => import('../src/backOffice/Backoffice'))
-const DetalleActividad= lazy(() => import('./Components/Activities/Detail/DetalleActividad'))
+const DetalleActividad = lazy(() => import('./Components/Activities/Detail/DetalleActividad'))
+
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <BrowserRouter>
         <Suspense fallback={<div>Loading...</div>}>
-          <Switch>
+          <AnimatedSwitch
+            atEnter={{ opacity: 0 }}
+            atLeave={{ opacity: 0 }}
+            atActive={{ opacity: 1 }}
+            className="fadeIn"
+          >
             {/* <Route path="/" exact component={} />           Esta ruta debe ser para el Home */}
             <Route path="/create-activity" component={ActivitiesForm} />
             <Route path="/create-category" component={CategoriesForm} />
@@ -45,7 +45,7 @@ function App() {
             <Route path="/Actividades" exact component={Actividades} />
             <Route path="/Actividades/:id" component={DetalleActividad} />
             <Route path="/backoffice" component={BackOficce} />
-          </Switch>
+          </AnimatedSwitch>
         </Suspense>
       </BrowserRouter>
     </ThemeProvider>
