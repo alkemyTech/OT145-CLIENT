a1f13091353cc0875729c4b3fa822c1eb503b1a2
Jesus Sivira Escobar <eduardosivira17@gmail.com>
2022-02-28 22:21:56 -0300

    Se documento el uso del Sweet alerts en el readme

--- src/Utils/AlertsProps.jsx
+++ src/Utils/AlertsProps.jsx
@@ -2,11 +2,12 @@ import Swal from "sweetalert2";
 import withReactContent from "sweetalert2-react-content";
 
 export const Modal = withReactContent(Swal);
+
+
 const showModal = (props) => {
   return Modal.fire({
     ...props,
     showCloseButton: true,
-    
   });
 };
 
