f3c208de2f3fafc71028a793ac58e4747ca16f58
Yesica Oviedo <oviedoyesica52@gmail.com>
2022-03-02 09:14:56 -0300

    corrijo errores en el app

--- src/App.js
+++ src/App.js
@@ -46,21 +46,6 @@ function App() {
   return (
     <ThemeProvider theme={theme}>
       <BrowserRouter>
-<<<<<<< HEAD
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/create-activity" component={ActivitiesForm} />
-          <Route path="/create-category" component={CategoriesForm} />
-          <Route path="/create-news" component={NewsForm} />
-          <Route path="/backoffice/create-slide" component={SlidesForm} />
-          <Route path="/create-testimonials" component={TestimonialForm} />
-          <Route path="/create-user" component={UserForm} />
-          <Route path="/create-member" component={MembersForm} />
-          <Route path="/create-project" component={ProjectsForm} />
-          <Route path="/school-campaign" component={SchoolCampaign} />
-          <Route path="/toys-campaign" component={ToysCampaign} />
-        </Switch>
-=======
         <Suspense fallback={<div>Loading...</div>}>
           <Switch>
             {/* <Route path="/" exact component={} />           Esta ruta debe ser para el Home */}
@@ -80,7 +65,6 @@ function App() {
             <Route exact path="/news" component={News} />
           </Switch>
         </Suspense>
->>>>>>> 1ceb45b80122940637331845cbbbc50cd60181d5
       </BrowserRouter>
     </ThemeProvider>
   )
