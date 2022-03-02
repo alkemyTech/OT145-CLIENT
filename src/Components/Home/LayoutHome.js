e1a92a006b6b09ef8d2dcc9ac35a7678f7cb665a
Yesica Oviedo <oviedoyesica52@gmail.com>
2022-03-02 14:00:24 -0300

    moved LayoutHome and StyledHeader

--- src/Components/Home/LayoutHome.js
+++ src/Components/Home/LayoutHome.js
@@ -1,6 +1,6 @@
 import React from "react";
-import Header from "./Header";
 import { Container } from "@mui/material";
+import Header from "./Header";
 
 const LayoutHome = (props) => {
     return(
