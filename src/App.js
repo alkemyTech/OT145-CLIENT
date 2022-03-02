69694de6ce9b5730ccdbc86bc5c24c3131035cc3
Yesica Oviedo <oviedoyesica52@gmail.com>
2022-03-02 09:32:07 -0300

    .

--- src/App.js
+++ src/App.js
@@ -1,19 +1,5 @@
 import React, { Suspense, lazy } from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
-<<<<<<< HEAD
-import Home from './Components/Home';
-import ActivitiesForm from './Components/Activities/ActivitiesForm'
-import CategoriesForm from './Components/Categories/CategoriesForm'
-import NewsForm from './Components/News/NewsForm'
-import SlidesForm from './Components/Slides/SlidesForm'
-import TestimonialForm from './Components/Testimonials/TestimonialsForm'
-import UserForm from './Components/Users/UsersForm'
-import SchoolCampaign from './Campaigns/School/SchoolCampaign'
-import ToysCampaign from './Campaigns/Toys/ToysCampaign'
-import MembersForm from './Components/Members/MembersForm'
-import ProjectsForm from './Components/Projects/ProjectsForm'
-=======
->>>>>>> 1ceb45b80122940637331845cbbbc50cd60181d5
 import { theme } from './theme'
 import { ThemeProvider } from '@mui/material'
 
@@ -46,21 +32,6 @@ function App() {
   return (
     <ThemeProvider theme={theme}>
       <BrowserRouter>
-<<<<<<< HEAD
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/create-activity" component={ActivitiesForm} />
-          <Route path="/create-category" component={CategoriesForm} />
-          <Route path="/create-news" component={NewsForm} />
-          <Route path="/backoffice/create-slide" component={SlidesForm} />
-          <Route path="/create-testimonials" component={TestimonialForm} />
-          <Route path="/create-user" component={UserForm} />
-          <Route path="/create-member" component={MembersForm} />
-          <Route path="/create-project" component={ProjectsForm} />
-          <Route path="/school-campaign" component={SchoolCampaign} />
-          <Route path="/toys-campaign" component={ToysCampaign} />
-        </Switch>
-=======
         <Suspense fallback={<div>Loading...</div>}>
           <Switch>
             {/* <Route path="/" exact component={} />           Esta ruta debe ser para el Home */}
@@ -80,7 +51,6 @@ function App() {
             <Route exact path="/news" component={News} />
           </Switch>
         </Suspense>
->>>>>>> 1ceb45b80122940637331845cbbbc50cd60181d5
       </BrowserRouter>
     </ThemeProvider>
   )
