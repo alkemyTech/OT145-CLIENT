9024c94f026f1dcf8d64c69c37480502f0608e32
Franco Jara <jarafranco122@gmail.com>
2022-03-08 19:02:05 -0300

    Se realizó el feedback para el home

--- src/Components/Home/Index.js
+++ src/Components/Home/Index.js
@@ -1,18 +1,28 @@
-import React from 'react';
-import HomeTitle from './HomeTitle';
+import React, { useState } from "react";
+import HomeTitle from "./HomeTitle";
 
+import Spinner from "../Spinner/Spinner";
+// import showModal from "../../Utils/AlertsProps";
 
 const Home = () => {
-    return(
-        <>
+	const [loading, setloading] = useState(true);
+	const [error, seterror] = useState(false);
 
-            <h2>Este seria el Slider</h2>
+	loading ? (
+		<Spinner />
+	) : (error ? (
+		<showModal />
+	) : (
+		<>
+			<h2>Este seria el Slider</h2>
 
-            <HomeTitle />
+			<HomeTitle />
 
-            <h2>Este seria el listado de Novedades</h2>
-        </>
-    )
-}
+			<h2>Este seria el listado de Novedades</h2>
+		</>
+	))
+
+	return null
+};
 
 export default Home;
