64d92733c1682a759e698ca1606519b57097c75b
AndyCambra <andycambra@gmail.com>
2022-03-11 12:51:06 -0300

    Se modificaron estilos

--- src/Components/Auth/AuthStyles.js
+++ src/Components/Auth/AuthStyles.js
@@ -3,6 +3,7 @@ import { makeStyles } from "@mui/styles";
 const useStyles = makeStyles((theme) => ({
     containerForm:{      
         padding: '10px',
+        maxWidth: '70%',
     },
     fieldForm:{
         marginTop: '10px !important',
