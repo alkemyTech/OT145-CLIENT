4c268da8dd3036ded36a474de384d883d99fb36f
Yesica Oviedo <oviedoyesica52@gmail.com>
2022-03-12 15:56:47 -0300

    arreglo formulariop y estilos

--- src/Components/ContactForm/StyleContactForm.js
+++ src/Components/ContactForm/StyleContactForm.js
@@ -2,39 +2,18 @@ import { makeStyles } from "@mui/styles";
 
 
 const useStyles = makeStyles((theme) => ({
-    container: {
-        display: "block",
-        maxWidth:"50%",
-        maxHeight:"500px",
-        boxShadow: "0 3px 5px 2px rgba(0, 0, 0, .3)",
-        backgroundColor: "white",  
-        borderRadius: "5px",
-        borderWidth: "1px",
-        borderStyle: "solid",
-        borderColor: "black",
-        boxSizing: "border-box",
-        margin:"auto",
-        marginTop:"10%",
-        [theme.breakpoints.down('sm')]: {
-            maxWidth:"90%",
-        },
+    containerForm:{      
+        padding: '10px',
+        maxWidth: '70%',
+    },
+    fieldForm:{
+        marginTop: '10px !important',
     },
     txt: {
-        padding: "3% 0 2% 5%",
-        [theme.breakpoints.down('sm')]: {
-            padding: "3% 3% 2% 5%"
-        },
-        
-    },
-    textArea:{
-        margin: "5% 0 8% 5%",
-        width:"90%",
-    },
-    btn: {
-        padding:"0 0 5% 40%",
-        [theme.breakpoints.down('sm')]: {
-            padding: "0 0 5% 35%"
-        },
+        marginTop: '10px !important',
+        marginBottom: '10px !important',
+        width:'99%',
+        maxHeight:'50%'
     }
 
 }));
