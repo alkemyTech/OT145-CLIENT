e9ae0bd6e7c1acb1612419b28c0dfbc7cbb89cfc
Franco Jara <jarafranco122@gmail.com>
2022-03-13 13:27:20 -0300

    Se agregó almacenamiento en localstorage y feedback

--- src/Components/Home/Newsletter.js
+++ src/Components/Home/Newsletter.js
@@ -1,11 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import { Container, Typography, TextField, Button } from "@mui/material";
 import { Box } from "@mui/system";
 import useStyles from "./styles/Newsletter";
 import { useFormik } from "formik";
+import useLocalStorage from "../../hooks/useLocalStorage";
 
 export default function Newsletter() {
 	const classes = useStyles();
+	const [item, setItem] = useLocalStorage("email", "");
+	const [suscribed, setSuscribed] = useState(false);
 
 	const formik = useFormik({
 		initialValues: {
@@ -13,30 +16,41 @@ export default function Newsletter() {
 		},
 		onSubmit: (values) => {
 			console.log(values);
+			setItem(values.email);
+			setSuscribed(true);
 		},
 	});
 
 	return (
 		<Box className={classes.newsletterContainer}>
 			<Container className={classes.content}>
-				<Typography variante="p" component="p">
-					Suscríbete a nuestro boletín
-				</Typography>
-				<form className={classes.form} onSubmit={formik.handleSubmit}>
-					<TextField
-						variant="outlined"
-						size="small"
-						label="Email"
-						// inputProps={{ id: "email", type: "email", name: "email" }}
-						id='email'
-						type='email'
-						name='email'
-						onChange={formik.handleChange}
-						value={formik.values.email}
-						sx={{ backgroundColor: "#fff" }}
-					/>
-					<Button type="submit" variant="contained">Enviar</Button>
-				</form>
+				{suscribed ? (
+					<Typography variante="p" component="p">
+						Te has suscrito exitosamente con {item}
+					</Typography>
+				) : (
+					<>
+						<Typography variante="p" component="p">
+							Suscríbete a nuestro boletín
+						</Typography>
+						<form className={classes.form} onSubmit={formik.handleSubmit}>
+							<TextField
+								variant="outlined"
+								size="small"
+								label="Email"
+								id="email"
+								type="email"
+								name="email"
+								onChange={formik.handleChange}
+								value={formik.values.email}
+								sx={{ backgroundColor: "#fff" }}
+							/>
+							<Button type="submit" variant="contained">
+								Enviar
+							</Button>
+						</form>
+					</>
+				)}
 			</Container>
 		</Box>
 	);
