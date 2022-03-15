15a501a6d1e4f0bcdf0b4ee64b7f6045bae83520
Yesica Oviedo <oviedoyesica52@gmail.com>
2022-03-14 21:13:37 -0300

    se instala ckeditor, y se modifican archivos SlidesForm y styleSlides

--- src/Components/Slides/SlidesForm.js
+++ src/Components/Slides/SlidesForm.js
@@ -1,10 +1,11 @@
 import React, { useState } from 'react';
 import { privatePATCH , privatePOST } from '../../Services/privateApiService'
-
 import { useFormik} from 'formik';
 import * as yup from 'yup';
-import { TextField, Button } from '@mui/material';
+import { TextField, Button, Typography } from '@mui/material';
 import useStyles from './styleSlides';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import {CKEditor} from '@ckeditor/ckeditor5-react';
 
 
 
@@ -16,19 +17,21 @@ const validationSchema = yup.object({
       .matches(/[a-zA-Z]/, 'El nombre solo puede tener letras'),
     description: yup
     .string('Ingrese Una descripcion')
-    .required('Es necesario ingresar un nombre'),
+    .required('Es necesario ingresar una descripcion'),
     order: yup
         .string('Ingrese orden')
+        .required('Es necesario ingresar un order')
         .matches(/(?=.*[0-9])/, 'Solo puede ingresar numeros'),
-    profilePhoto: yup
-    .mixed()
-    .test(
-        "type",
-        "Solo imagenes png y jpg",
-        (value) =>{
-            return value && ([".jpg"].includes(value) || [".png"].includes(value))}
-        )
-    .required('Es necesario ingresar una imagen'),
+    image: yup
+        .mixed()
+        .test(
+            "type",
+            "Solo imagenes png y jpg",
+
+            (value) =>{
+                return value && (["image/jpg"].includes(value.type) || ["image/png"].includes(value.type))}
+            )
+        .required('Es necesario ingresar una imagen'),
     
   });
 
@@ -54,32 +57,52 @@ const SlidesForm = ({ data }) => {
                 privatePOST('https://ongapi.alkemy.org/api/slides', values);
             }
         },
-        
+   
     });
 
-    
+
     return (
-        <div>
-            <form onSubmit={formik.handleSubmit} className={classes.containerForm}>
+        <div >
+            <form onSubmit={formik.handleSubmit}  className={classes.containerForm}>
                 <TextField fullWidth
                     id="name" 
                     name="name" 
+                    placeholder="Nombre"
                     className={classes.fieldForm}
                     value={formik.values.name} 
                     onChange={formik.handleChange}
                     error={formik.touched.name && Boolean(formik.errors.name)}
                     helperText={formik.touched.name && formik.errors.name}/>
 
+                <TextField fullWidth
+                    id="order"
+                    name="order" 
+                    type="number"
+                    className={classes.fieldForm}
+                    value={formik.values.order} 
+                    onChange={formik.handleChange} placeholder="Order"                     
+                    error={formik.touched.order && Boolean(formik.errors.order)}
+                    helperText={formik.touched.order && formik.errors.order}/>
 
                 <TextField fullWidth
-                    id="description"
-                    name="description" 
+                    id="image"
+                    name="image" 
+                    type="file"
                     className={classes.fieldForm}
-                    value={formik.values.description} 
-                    onChange={formik.handleChange} placeholder="Descripcion"                     
-                    error={formik.touched.name && Boolean(formik.errors.name)}
-                    helperText={formik.touched.name && formik.errors.name}/>
+                    onChange={(e)=>formik.setFieldValue("image", e.target.files[0])} placeholder="Descripcion"                     
+                    error={formik.touched.image && Boolean(formik.errors.image)}
+                    helperText={formik.touched.image && formik.errors.image}/>
 
+                <div className={classes.fieldForm}>
+                    <CKEditor
+                        id="description"
+                        name="description"
+                        onChange={(event, editor)=>formik.setFieldValue("description", editor.getData())}
+                        editor={ClassicEditor}/>
+                    {formik.handleSubmit && formik.errors.description &&
+                        <Typography variant="caption" color="error">Es necesario ingresar una descripcion</Typography>
+                    }
+                </div>
                 <Button color="secondary" variant="contained" fullWidth type="submit">
                     Enviar
                 </Button>
@@ -87,5 +110,4 @@ const SlidesForm = ({ data }) => {
         </div>
     );
 }
- 
 export default SlidesForm;
--- src/Components/Slides/styleSlides.js
+++ src/Components/Slides/styleSlides.js
@@ -3,7 +3,7 @@ import { makeStyles } from "@mui/styles";
 const useStyles = makeStyles((theme) => ({
     containerForm:{      
         padding: '10px',
-        maxWidth: '70%',
+        maxWidth: '50%',
         margin:'auto'
     },
     fieldForm:{
