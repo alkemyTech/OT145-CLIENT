02aaab1e883751f2c4f47a26813e7fc1fed71743
Yesica Oviedo <oviedoyesica52@gmail.com>
2022-03-16 20:31:45 -0300

    se cambia UserForm.js

--- src/Components/Users/UsersForm.js
+++ src/Components/Users/UsersForm.js
@@ -1,126 +1,98 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useFormik} from 'formik';
-import * as yup from 'yup';
+import { validationSchema } from './config/index';
 import {Button , TextField, Autocomplete, Typography } from '@mui/material';
 import useStyles from './style';
 import { privatePATCH, privatePOST} from '../../Services/privateApiService';
+import { convertToBase64 } from '../News/config/helper';
 
 
-const validationSchema = yup.object({
-    email: yup
-      .string('Ingrese su mail')
-      .email('Ingrese una dirección de mail válida')
-      .required('Es necesario ingresar una dirección de mail'),
-    name: yup
-      .string('Ingrese su nombre')
-      .min(4, 'El nombre debe tener al menos 4 caracteres')
-      .required('Es necesario ingresar un nombre')
-      .matches(/[a-zA-Z]/, 'El nombre solo puede tener letras'),
-    role_id: yup
-        .string('Selecione un rol')
-        .required('Elija una opción'),
-    password: yup
-        .string('Ingrese su contraseña')
-        .min(8, 'La contraseña debe tener una longitud mínima de 8 caraceteres.')
-        .required('Es necesario ingresar una contraseña'),
-    profile_image: yup
-        .mixed()
-        .test(
-            "type",
-            "Solo imagenes png y jpg",
-            (value) =>{
-                return value && (["image/jpg"].includes(value.type) || ["image/png"].includes(value.type))}
-            )
-        .required('Es necesario ingresar una imagen'),
-});
 
 
 
 
 const UserForm = ({ data }) => {
-    const classes = useStyles()
-    const options = ["", "Administrador", "Regular"]
-    const [userData, setUserData] = useState(data || {
-        name: '',
-        email: '',
-        role_id: '',
-        password:'',
-        profile_image:''
-    }); 
-    const formik = useFormik({
-        initialValues:{
-            ...userData
-        },
+    const classes = useStyles();
+
+    const options = [0,1]
+
+    const initialValues = {
+        name: data?.name || '',
+        email: data?.email || '',
+        role_id: data?.role_id || '',
+        password: data?.password || '',
+        profile_image: data?.profile_image || ''
+    }; 
+
+    const { handleSubmit, handleChange, handleBlur, touched, errors, setFieldValue, values } = useFormik({
+        initialValues: initialValues,
         validationSchema: validationSchema,
-        onSubmit: (values) => {
-            if(data){
-                privatePATCH('https://ongapi.alkemy.org/api/users', data.id, values)
-            } else {
+        onSubmit: ( async (values) => {
+            if (data) {
+                const base64 = await convertToBase64(values.profile_image)
+                values.profile_image = base64
+                privatePATCH(`https://ongapi.alkemy.org/api/users/${data.id}`, values);
+            }
+            else {
+                const base64 = await convertToBase64(values.profile_image)
+                values.profile_image = base64
                 privatePOST('https://ongapi.alkemy.org/api/users', values);
             }
-        },
-    });
+        })
+    })
 
     return (
         <div className={classes.containerForm}>
             <Typography variant='h6'>Registrate</Typography>
-            <form onSubmit={formik.handleSubmit}>
+            <form onSubmit={handleSubmit}>
                 <TextField
                     fullWidth
-                    id="name"
                     name="name"
                     label="Nombre"
                     className={classes.fieldForm}
-                    value={formik.values.name}
-                    onChange={formik.handleChange}
-                    error={formik.touched.name && Boolean(formik.errors.name)}
-                    helperText={formik.touched.name && formik.errors.name}
-                    color="secondary"
+                    value={values.name}
+                    onChange={handleChange}
+                    error={touched.name && Boolean(errors.name)}
+                    helperText={touched.name && errors.name}
                 />
                 <TextField
-                   
                     fullWidth
-                    id="email"
                     name="email"
                     label="Email"
                     className={classes.fieldForm}
-                    value={formik.values.email}
-                    onChange={formik.handleChange}
-                    error={formik.touched.email && Boolean(formik.errors.email)}
-                    helperText={formik.touched.email && formik.errors.email}
-                    color="secondary"
+                    value={values.email}
+                    onChange={handleChange}
+                    error={touched.email && Boolean(errors.email)}
+                    helperText={touched.email && errors.email}
                 />
                 <TextField className={classes.fieldForm}
                     fullWidth
-                    id="password"
                     name="password"
                     label="Contraseña"
                     type="password"
-                    value={formik.values.password}
-                    onChange={formik.handleChange}
-                    error={formik.touched.password && Boolean(formik.errors.password)}
-                    helperText={formik.touched.password && formik.errors.password}
-                    color="secondary"
+                    value={values.password}
+                    onChange={handleChange}
+                    error={touched.password && Boolean(errors.password)}
+                    helperText={touched.password && errors.password}
                 />
                 <Autocomplete
-                    id="role_id"
                     name="role_id"
                     className={classes.txt}
-                    value={formik.values.role_id}
+                    value={values.role_id}
                     options={options}
-                    onChange={(e, value) => formik.setFieldValue("role_id", value)}
-                    renderInput={(params) => <TextField {...params} label="Elija una opcion" error={formik.touched.role_id && Boolean(formik.errors.role_id)} helperText={formik.touched.role_id && formik.errors.role_id}/>}
+                    onBlur={handleBlur}
+                    onChange={(e, value) => setFieldValue("role_id", value)}
+                    renderInput={(params) => <TextField {...params} label="Elija una opcion" error={touched.role_id && Boolean(errors.role_id)} helperText={touched.role_id && errors.role_id}/>}
                 />
                 <TextField className={classes.fieldForm}
                     fullWidth
-                    id="profile_image"
                     name="profile_image"
                     type="file"
-                    onChange={(e)=>formik.setFieldValue("profile_image", e.target.files[0])}
-                    error={formik.touched.profile_image && Boolean(formik.errors.profile_image)}
-                    helperText={formik.touched.profile_image && formik.errors.profile_image}/>
+                    onChange={(e)=>setFieldValue("profile_image", e.target.files[0])}
+                    error={touched.profile_image && Boolean(errors.profile_image)}
+                    helperText={touched.profile_image && errors.profile_image}/>
 
-                <Button color="secondary" variant="contained" fullWidth type="submit" onSubmit={formik.handleSubmit}>
+                <Button color="secondary" variant="contained" fullWidth type="submit" onSubmit={handleSubmit}>
                     Enviar
                 </Button>
             </form>
