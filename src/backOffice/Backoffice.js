aebec07d9962f0b98ab33e174ffbe9e60868a7a9
Jesus Sivira Escobar <eduardosivira17@gmail.com>
2022-03-15 22:41:31 -0300

    Se protegieron las rutas del backoffice con localstorage

--- src/backOffice/Backoffice.js
+++ src/backOffice/Backoffice.js
@@ -1,11 +1,12 @@
 import React, { lazy, useState } from 'react'
-import { Route, Switch, useRouteMatch } from 'react-router-dom'
+import { Route, useRouteMatch, Switch } from 'react-router-dom';
 import CategoriesList from './Categories'
 import Header from './Header'
-import Sidebar from './Sidebar'
+import SideBar from './SideBar'
 import NewsList from './NewsList/NewsList';
 import UsersList from './UsersList'
 import Error404 from '../Components/Error404/Error404'
+import PrivateRoute from './PrivateRoutes/PrivateRoute'
 
 
 const ActivitiesForm = lazy(() =>
@@ -39,25 +40,25 @@ function BackOficce() {
   return (
     <>
       <Header open={open} setOpen={setOpen} />
-      <Sidebar open={open} />
+      <SideBar open={open} />
       <Switch>
-        <Route exact path={`${match.path}/news`} component={NewsList} />
-        <Route exact path={`${match.path}/news/create-news`} component={NewsForm} />
-        <Route exact path={`${match.path}`} component={ScreenDashboard} />
-        <Route exact path={`${match.path}/create-category`} component={CategoriesForm} />
-        <Route exact path={`${match.path}/slides/create`} component={SlidesForm} />
-        <Route exact path={`${match.path}/create-testimonials`} component={TestimonialForm} />
-        <Route exact path={`${match.path}/users/create-user`} component={UserForm} />
-        <Route exact path={`${match.path}/members/edit`} component={MembersForm} />
-        <Route exact path={`${match.path}/create-project`} component={ProjectsForm} />
-        <Route exact path={`${match.path}/slides`} component={Slides} />
-        <Route exact path={`${match.path}/organization`} component={OrganizationScreen} />
-        <Route exact path={`${match.path}/categories`} component={CategoriesList} />
-        <Route exact path={`${match.path}/activities`} component={ActivitiesList} />
-        <Route exact path={`${match.path}/activities/create-activity`} component={ActivitiesForm} />
-        <Route exact path={`${match.path}/users`} component={UsersList} />
-        <Route exact path={`${match.path}/members`} component={MemberList} />
-        <Route path="*" component={Error404} />
+        <PrivateRoute exact path={`${match.path}/news`} component={NewsList} />
+        <PrivateRoute exact path={`${match.path}/news/create-news`} component={NewsForm} />
+        <PrivateRoute exact path={`${match.path}`} component={ScreenDashboard} />
+        <PrivateRoute exact path={`${match.path}/create-category`} component={CategoriesForm} />
+        <PrivateRoute exact path={`${match.path}/slides/create`} component={SlidesForm} />
+        <PrivateRoute exact path={`${match.path}/create-testimonials`} component={TestimonialForm} />
+        <PrivateRoute exact path={`${match.path}/users/create-user`} component={UserForm} />
+        <PrivateRoute exact path={`${match.path}/members/edit`} component={MembersForm} />
+        <PrivateRoute exact path={`${match.path}/create-project`} component={ProjectsForm} />
+        <PrivateRoute exact path={`${match.path}/slides`} component={Slides} />
+        <PrivateRoute exact path={`${match.path}/organization`} component={OrganizationScreen} />
+        <PrivateRoute exact path={`${match.path}/categories`} component={CategoriesList} />
+        <PrivateRoute exact path={`${match.path}/activities`} component={ActivitiesList} />
+        <PrivateRoute exact path={`${match.path}/activities/create-activity`} component={ActivitiesForm} />
+        <PrivateRoute exact path={`${match.path}/users`} component={UsersList} />
+        <PrivateRoute exact path={`${match.path}/members`} component={MemberList} />
+        <PrivateRoute path="*" component={Error404} />
       </Switch>
     </>
   )
