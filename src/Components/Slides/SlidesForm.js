3d8a2bed28614b0e5dbd7c80d5f9ec79037777af
Yesica Oviedo <oviedoyesica52@gmail.com>
2022-03-17 10:16:38 -0300

    se modifica SlidesForm.js y se crea carpeta config e index.js

--- src/Components/Slides/SlidesForm.js
+++ src/Components/Slides/SlidesForm.js
@@ -1,107 +1,81 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { privatePATCH , privatePOST } from '../../Services/privateApiService'
 import { useFormik} from 'formik';
-import * as yup from 'yup';
+import { validationSchema } from './config/index';
+import { convertToBase64 } from '../News/config/helper';
 import { TextField, Button, Typography } from '@mui/material';
 import useStyles from './styleSlides';
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 import {CKEditor} from '@ckeditor/ckeditor5-react';
 
 
-
-const validationSchema = yup.object({
-    name: yup
-      .string('Ingrese su nombre')
-      .min(4, 'El nombre debe tener al menos 4 caracteres')
-      .required('Es necesario ingresar un nombre')
-      .matches(/[a-zA-Z]/, 'El nombre solo puede tener letras'),
-    description: yup
-    .string('Ingrese Una descripcion')
-    .required('Es necesario ingresar una descripcion'),
-    order: yup
-        .string('Ingrese orden')
-        .required('Es necesario ingresar un order')
-        .matches(/(?=.*[0-9])/, 'Solo puede ingresar numeros'),
-    image: yup
-        .mixed()
-        .test(
-            "type",
-            "Solo imagenes png y jpg",
-
-            (value) =>{
-                return value && (["image/jpg"].includes(value.type) || ["image/png"].includes(value.type))}
-            )
-        .required('Es necesario ingresar una imagen'),
-    
-  });
-
 const SlidesForm = ({ data }) => {
     const classes = useStyles();
 
-    const [slidesData, setSlidesData] = useState(data || {
-        name: '',
-        description: '',
-        order:0,
-        image:''
-    });
+    const initialValues = {
+        name: data?.name || '',
+        description: data?.description || '',
+        order: data?.order || 0,
+        image: data?.image || ''
+    };
 
-    const formik = useFormik({
-        initialValues:{
-            ...slidesData
-        },
+    const { handleSubmit, handleChange, handleBlur, touched, errors, setFieldValue, values } = useFormik({
+        initialValues: initialValues,
         validationSchema: validationSchema,
-        onSubmit: (values) => {
-            if(data){
-                privatePATCH('https://ongapi.alkemy.org/api/slides', data.id, values)
-            } else {
+        onSubmit: ( async (values) => {
+            if (data) {
+                const base64 = await convertToBase64(values.image)
+                values.image = base64
+                privatePATCH('https://ongapi.alkemy.org/api/slides', data.id, values);
+            }
+            else {
+                const base64 = await convertToBase64(values.image)
+                values.image = base64
                 privatePOST('https://ongapi.alkemy.org/api/slides', values);
             }
-        },
-   
-    });
-
+        })
+    })
 
     return (
         <div  className={classes.containerForm}>
             <Typography variant="h6">Crear slide</Typography>
-            <form onSubmit={formik.handleSubmit}>
+            <form onSubmit={handleSubmit}>
                 <TextField fullWidth
-                    id="name" 
                     name="name" 
+                    type="text"
                     placeholder="Nombre"
                     className={classes.fieldForm}
-                    value={formik.values.name} 
-                    onChange={formik.handleChange}
-                    error={formik.touched.name && Boolean(formik.errors.name)}
-                    helperText={formik.touched.name && formik.errors.name}/>
+                    value={values.name} 
+                    onChange={handleChange}
+                    error={touched.name && Boolean(errors.name)}
+                    helperText={touched.name && errors.name}/>
 
                 <TextField fullWidth
-                    id="order"
                     name="order" 
                     type="number"
                     className={classes.fieldForm}
-                    value={formik.values.order} 
-                    onChange={formik.handleChange} placeholder="Order"                     
-                    error={formik.touched.order && Boolean(formik.errors.order)}
-                    helperText={formik.touched.order && formik.errors.order}/>
+                    value={values.order} 
+                    onChange={handleChange} 
+                    placeholder="Order"                     
+                    error={touched.order && Boolean(errors.order)}
+                    helperText={touched.order && errors.order}/>
 
                 <TextField fullWidth
-                    id="image"
                     name="image" 
                     type="file"
                     className={classes.fieldForm}
-                    onChange={(e)=>formik.setFieldValue("image", e.target.files[0])}                     
-                    error={formik.touched.image && Boolean(formik.errors.image)}
-                    helperText={formik.touched.image && formik.errors.image}/>
+                    onChange={(e)=>setFieldValue("image", e.target.files[0])}                     
+                    error={touched.image && Boolean(errors.image)}
+                    helperText={touched.image && errors.image}/>
 
                 <div className={classes.fieldForm}>
                     <CKEditor
                         id="description"
                         name="description"
-                        onChange={(event, editor)=>formik.setFieldValue("description", editor.getData())}
+                        onChange={(event, editor)=>setFieldValue("description", editor.getData())}
                         editor={ClassicEditor}/>
-                    {formik.handleSubmit && formik.errors.description &&
-                        <Typography variant="caption" color="error" className={classes.typography}>Es necesario ingresar una descripcion</Typography>
+                    {handleSubmit && errors.description &&  
+                        <Typography variant="caption" color="error">{touched.description && errors.description}</Typography> 
                     }
                 </div>
                 <Button color="secondary" variant="contained" fullWidth type="submit">
