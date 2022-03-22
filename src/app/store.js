e3238282f8d3dac4d1843b3083c276ce2a07ac9e
Yesica Oviedo <oviedoyesica52@gmail.com>
2022-03-22 13:09:49 -0300

    .

--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from "../redux/authReducer"
+import categorySlice from '../redux/Categories/categorySlice'
 
 export default configureStore({
   reducer: {
