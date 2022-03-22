9b1dfca459f7543c60745de437e4875be6f4e7ed
Yesica Oviedo <oviedoyesica52@gmail.com>
2022-03-22 13:53:59 -0300

    se terminan las peticiones http

--- src/Peticiones/categoriesService.js
+++ src/Peticiones/categoriesService.js
@@ -1,15 +1,22 @@
 import privateGET, { privateDelete, privatePOST, privatePUT } from "../Services/privateApiService";
-/*
-const endPointGet = process.env.REACT_APP_API_GET_CATEGORIES
-const endPointGetID=process.env.REACT_APP_API_GET_CATEGORIES_ID
-const endPointPost=process.env.REACT_APP_API_POST_CATEGORIES
-const endPointDelete=process.env.REACT_APP_API__DELETE_CATEGORIES
-const endPointPut=process.env.REACT_APP_API_PUT_CATEGORIES
-*/
-
 const endPoint=process.env.REACT_APP_API_CATEGORIES
 
 export const getCATEGORIES = ()=>{
     return privateGET(endPoint)
 }
 
+export const getCATEGORIESID = (id)=>{
+    return privateGET(endPoint, id)
+}
+
+export const postCategory = (data)=>{
+    return privatePOST(endPoint,data)
+}
+
+export const deleteCategory = (id)=>{
+    return privateDelete(endPoint,id);
+}
+
+export const putCategory = (id,data)=>{
+    return privatePUT(endPoint,id,data)
+}
