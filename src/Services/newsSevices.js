9fc24e5e2d09f402be3f6f7c661e832172886f8f
andy <andycambra@gmail.com>
2022-03-22 15:02:29 -0300

    Se agrego en News Service

---
+++ src/Services/newsSevices.js
@@ -0,0 +1,29 @@
+import privateGET, {
+  privateDelete,
+  privatePATCH,
+  privatePUT,
+  privatePOST,
+} from './privateApiService'
+
+const NEWS_URL = process.env.REACT_APP_API_GET_NEWS
+
+export const getAllNews = () => {
+  return privateGET(NEWS_URL)
+}
+
+export const getNewsById = (id) => {
+  return privateGET(NEWS_URL, id)
+}
+
+export const postNews = (body) => {
+  return privatePOST(NEWS_URL, body)
+}
+export const patchNews = (id, body) => {
+  return privatePATCH(NEWS_URL, id, body)
+}
+export const deleteNews = (id) => {
+  return privateDelete(NEWS_URL, id)
+}
+export const putNews = (id, body) => {
+  return privatePUT(NEWS_URL, id, body)
+}
