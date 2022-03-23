7d8f70a1c3dc6a0080f2eaf9022362949a80a8db
Yesica Oviedo <oviedoyesica52@gmail.com>
2022-03-23 18:42:18 -0300

    .

--- src/Utils/sweetAlertConfirm.js
+++ src/Utils/sweetAlertConfirm.js
@@ -1,20 +1,22 @@
-import Swal from "sweetalert2";
+import Swal from 'sweetalert2';
 
-export const SweetAlertConfirm = async ()=> {
+
+export const sweetAlertConfirm = async () => {
     const alerta = Swal.fire({
-        title: "Estas seguro?",
-        text: "Si lo borras, no podras recuperarlo!",
-        icon: "warning",
-        showCancelButton: true,
-        confirmButtonColor: "#3085d6",
-        cancelButtonColor: "#d33",
-        confirmButtonText: "Si, Borrar!",
+      title: "Estas Seguro?",
+      text: "Si lo borras, no podras recuperarlo!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Si, Borrar!",
     });
 
-    const response = await alerta;
-    if(response.isConfirmed){
-        Swal.fire("Eliminado!", "Se elimino correctamente.", "success");
-    }
-    
+     const response = await alerta;
+
+     if(response.isConfirmed){
+      Swal.fire("Eliminado!", "Se elimino correctamente.", "success");
+     }
+
     return response.isConfirmed;
 }
