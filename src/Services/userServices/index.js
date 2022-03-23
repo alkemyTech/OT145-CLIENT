adcfad36898541d643567456508fec6de6d97264
Gabriel Barrientos <gabrielbarrientos729@gmail.com>
2022-03-22 23:37:52 -0300

    se modifico valores

--- src/Services/userServices/index.js
+++ src/Services/userServices/index.js
@@ -1,27 +1,24 @@
 import {privateGET,privateDelete,privatePATCH,privatePUT, privatePOST} from "../../Services/privateApiService"
 
-const endPointGet = process.env.REACT_APP_API_GET_USER
-const endPointGetID = process.env.REACT_APP_API_GET_USER_ID
-const endPointPost= process.env.REACT_APP_API_POST_USER
-const endPointDelete= process.env.REACT_APP_API_PUT_USER
-const endPointPut= process.env.REACT_APP_API_DELETE_USER
+
+const USER_URL = process.env.REACT_APP_API_GET_USER
 
 export const getUser = () => {
-    return privateGET(endPointGet)
+    return privateGET(USER_URL)
 }
 
 export const getUserID = (id) => {
-    return privateGET(endPointGetID,id)
+    return privateGET(`${USER_URL}/${id}`)
 }
 
-export const postUser = (data) => {
-    return privatePOST(endPointPost,data)
+export const postUser = (body) => {
+    return privatePOST(USER_URL,body)
 }
 
 export const deleteUser = (id) => {
-    return privateDelete(endPointDelete,id)
+    return privateDelete(USER_URL,id)
 }
 
-export const putUser = (id,data) => {
-    return privatePUT(endPointPut,id,data)
+export const putUser = (id,body) => {
+    return privatePUT(USER_URL,id,body)
 }
