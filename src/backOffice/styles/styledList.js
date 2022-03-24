8c872a76e24d4147b39980ef0db3c886ad369c33
PlazaFer <plazafernando9222@gmail.com>
2022-03-24 17:24:07 -0300

    Se agrega boton para navegar, se modifica forma de actualizar con delete, se maneja el error en getmemberbyid

--- src/backOffice/styles/styledList.js
+++ src/backOffice/styles/styledList.js
@@ -43,11 +43,17 @@ const useStyles = makeStyles((theme) => ({
     },
     title:{
         textAlign: 'center',
-        padding: '20px 0px 20px 0px'
+        paddingBottom: 20
     },
     icon:{
         color: theme.palette.secondary.main,
         cursor: 'pointer'
+    },
+    buttonBack:{
+        margin: '15px 0px 0px 15px'
+    },
+    iconButtonBack:{
+        fontSize: 30
     }
 }));
 
