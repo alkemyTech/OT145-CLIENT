e338053d0339eb8e58dd35c19f0071c3d267e887
Jesus Sivira Escobar <eduardosivira17@gmail.com>
2022-03-25 22:20:53 -0300

    Final final de rutas protegidas sin render raro

--- src/Components/Home/Routes.js
+++ src/Components/Home/Routes.js
@@ -2,7 +2,7 @@ import React, { lazy } from "react";
 import { Route, Switch, useRouteMatch, Redirect } from "react-router-dom";
 import LayoutHome from "./LayoutHome";
 import Error404 from "../../shared/Error404/Error404";
-import { isLogin } from "../../Services/isUserLogged";
+import PrivateRoutes from '../../backOffice/PrivateRoutes'
 import { Login } from "@mui/icons-material";
 // const Donacion = lazy(() => import('./Components/Donations/Donacion'))
 // const Gracias = lazy(() => import('./Components/Donations/Gracias'))
@@ -31,10 +31,10 @@ export default function Routes() {
         <Route exact path="/news" component={News} />
         <Route exact path="/contacto" component={Contacto} />
         <Route exact path="/login" component={LoginForm} />
-        <Route
+        <PrivateRoutes
           exact
           path="/register"
-          component={isLogin() ? Home : RegisterForm}
+          component={RegisterForm}
         />
         <Route path="*" component={Error404} />
       </Switch>
--- src/backOffice/PrivateRoutes.jsx
+++ src/backOffice/PrivateRoutes.jsx
@@ -1,21 +1,51 @@
 import { Route, Redirect } from 'react-router-dom'
 import { useSelector,useDispatch } from 'react-redux'
-import { useEffect } from 'react'
+import { useEffect, useState} from 'react'
 import { authMe } from '../redux/usersReducer/action'
-const PrivateRoutes = ({ component: Component, rol, ...rest }) => {
+const PrivateRoutes = ({ component: Component, path,rol,  ...rest }) => {
   const dispatch = useDispatch();
   const { rol_type, user,token } = useSelector(state => state.auth)
 
-  useEffect(()=> {
+  let entrar = false;
+  console.log(path)
+
+  /*useEffect(()=> {
     dispatch(authMe(token))
-  },[])
+  },[])*/
+
+  switch (rol_type) {
+    case 'Admin':
+        if (path === '/backoffice' && user) {
+          entrar = true
+        }
+        else if (path !== '/backoffice' && user) {
+          entrar = false
+        }
+        break;
+      case 'Standart':
+        if (path === '/backoffice' && user) {
+          entrar = false
+        }
+        else if (path !== '/backoffice' && user) {
+          entrar = false
+        }
+        break;
+        case '': 
+          if (path === '/backoffice' || !user) {
+            entrar = false
+          }
+      default:
+        if (path !== '/backoffice' || !user) {
+          entrar = true
+        }
+  }
 
   return (
     <Route
       {...rest}
       render={
         (props) => (
-          (rol_type === rol && user )
+          (entrar)
             ? (
               <Component {...props} />
             ) : (
