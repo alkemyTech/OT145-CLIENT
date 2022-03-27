2273a5aa185e4b8f931ecc4b68297eebd968d191
Yesica Oviedo <oviedoyesica52@gmail.com>
2022-03-27 19:31:01 -0300

    SE MODIFICAN ESTILOS DE FORMULARIO USUARIOS

--- src/backOffice/components/Categories/CategoriesForm.js
+++ src/backOffice/components/Categories/CategoriesForm.js
@@ -91,6 +91,7 @@ const CategoriesForm = () => {
 			</IconButton>
 			<form onSubmit={handleSubmit} className={classes.form}>
 				<Paper className={classes.paper} elevation={5}>
+					<Typography className={classes.title} variant="h5">{categoriesById ? 'Editar Usuario' : 'Crear Usuario'}</Typography>
 					<TextField
 						className={classes.inputs}
 						name="name"
--- src/backOffice/components/Users/UsersForm.js
+++ src/backOffice/components/Users/UsersForm.js
@@ -6,11 +6,10 @@ import { useLocation, useHistory } from 'react-router-dom';
 import { postUser, putUser } from '../../../redux/Users/userSlice';
 import { convertToBase64 } from '../../../helpers/base64';
 import { getUsersById } from '../../../redux/Users/userSlice';
-// import { registarUsuario } from '../../../redux/usersReducer/action';
-import {Button , TextField, Typography, Select, MenuItem, InputLabel, FormControl, FormHelperText, IconButton} from '@mui/material';
+import {Button , TextField, Typography, Select, MenuItem, InputLabel, FormControl, FormHelperText, IconButton, Container, Paper} from '@mui/material';
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import Spinner from '../../../shared/Spinner/Spinner';
-import useStyles from './style';
+import useStyles from '../../styles/newsFormStyles'
 
 const validationSchema = yup.object({
     email: yup
@@ -94,24 +93,24 @@ const UserForm = () => {
         }
     }, [status])
 
-    return (
-    <>
-    <IconButton 
-      aria-label="upload picture" 
-      component="span" 
-      className={classes.buttonBack} 
-      onClick={() => history.push('/backoffice/users')}
-    >
-      <ArrowBackIcon className={classes.iconButtonBack} />
-    </IconButton>
-        <div className={classes.containerForm}>
-            <Typography variant='h6'>{userId ? 'Editar Usuario' : 'Crear Usuario'}</Typography>
-            <form onSubmit={handleSubmit}>
+return (
+    <Container className={classes.container}>
+        <IconButton 
+        aria-label="upload picture" 
+        component="span" 
+        className={classes.buttonBack} 
+        onClick={() => history.push('/backoffice/users')}
+        >
+        <ArrowBackIcon className={classes.iconButtonBack} />
+        </IconButton>
+        <form onSubmit={handleSubmit} className={classes.form}>
+            <Paper className={classes.paper} elevation={5}>
+                <Typography className={classes.title} variant="h5">{userId ? 'Editar Usuario' : 'Crear Usuario'}</Typography>
                 <TextField
                     fullWidth
                     name="name"
                     label="Nombre"
-                    className={classes.fieldForm}
+                    className={classes.inputs}
                     value={values.name}
                     onChange={handleChange}
                     onBlur={handleBlur}
@@ -123,7 +122,7 @@ const UserForm = () => {
                     fullWidth
                     name="email"
                     label="Email"
-                    className={classes.fieldForm}
+                    className={classes.inputs}
                     value={values.email}
                     onChange={handleChange}
                     onBlur={handleBlur}
@@ -131,7 +130,7 @@ const UserForm = () => {
                     helperText={touched.email && errors.email}
                     color="secondary"
                 />
-                <TextField className={classes.fieldForm}
+                <TextField className={classes.inputs}
                     fullWidth
                     name="password"
                     label="Contraseña"
@@ -163,8 +162,8 @@ const UserForm = () => {
                     </Select>
                     {errors.role_id && <FormHelperText>{errors.role_id}</FormHelperText>}
                 </FormControl>
-               
-                <TextField className={classes.fieldForm}
+            
+                <TextField className={classes.inputs}
                     fullWidth
                     name="profile_image"
                     type="file"
@@ -179,9 +178,9 @@ const UserForm = () => {
                         : 'Enviar'    
                     }
                 </Button>
-            </form>
-        </div>
-        </>
+            </Paper>
+        </form>
+    </Container>
     );
 }
  
