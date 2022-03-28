eafb7c3e70a22a25367f7021ede9d629209fbe05
Yesica Oviedo <oviedoyesica52@gmail.com>
2022-03-27 22:09:27 -0300

    .

--- src/backOffice/styles/newsFormStyles.js
+++ src/backOffice/styles/newsFormStyles.js
@@ -2,15 +2,15 @@ import { makeStyles } from '@mui/styles'
 
 const useStyles = makeStyles((theme) => ({
   container: {
-    marginTop: theme.spacing(1),
-    [theme.breakpoints.down('md')]: {
-      width:"100vw",
-    },
+    marginTop: theme.spacing(5),
   },
   form: {
     padding: '10px',
     maxWidth: '70%',
     margin: 'auto',
+    [theme.breakpoints.down('md')]: {
+      maxWidth: '100%',
+    },
   },
   paper: {
     padding: theme.spacing(5),
