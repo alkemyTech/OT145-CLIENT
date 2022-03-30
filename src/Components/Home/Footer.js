27d644a70ed2f1cb7ecc3f41e04c2105270fa260
Franco Jara <jarafranco122@gmail.com>
2022-03-30 16:00:02 -0300

    Se cambió estructura de footer

--- src/Components/Home/Footer.js
+++ src/Components/Home/Footer.js
@@ -3,91 +3,104 @@ import useStyles from "./styles/footerStyles";
 import { Box } from "@mui/system";
 import { Container, Typography } from "@mui/material";
 import { NavLink } from "react-router-dom";
-import Newsletter from './Newsletter';
+import Newsletter from "./Newsletter";
 import { useSelector } from "react-redux";
-
+import Linkedin from "../../image/socialMediaIcons/linkedin.svg";
+import Instagram from "../../image/socialMediaIcons/instagram.svg";
+import Twitter from "../../image/socialMediaIcons/twitter.svg";
+import Facebook from "../../image/socialMediaIcons/facebook.svg";
 
 export default function Footer() {
-	const {isLogin} = useSelector(state => state.auth)
-	function getData() {
-		// Obtener datos de endpoint /organization utilizando los servicios reutilizables.
+	const { isLogin } = useSelector((state) => state.auth);
 
-		// Mockup parcial
-		const data = {
-			socialMedia: [
-				{
-					name: "Facebook",
-					url: "https://facebook.com",
-				},
-				{
-					name: "LinkedIn",
-					url: "https://linkedin.com",
-				},
-				{
-					name: "Instagram",
-					url: "https://instagram.com",
-				},
-			],
-			navigationItems: [
-				{
-					name: "Actividades",
-					url: "/activities",
-				},
-				{
-					name: "Novedades",
-					url: "/news",
-				},
-			],
-		};
-		return data;
-	}
+	const navigationItemsLeft = [
+		{
+			name: "Actividades",
+			url: "/activities",
+		},
+		{
+			name: "Novedades",
+			url: "/news",
+		},
+	];
 
-	const footerData = getData();
+	const navigationItemsRight = [
+		{
+			name: "Contacto",
+			url: "/contacto",
+		},
+		{
+			name: "Nosotros",
+			url: "/nosotros",
+		},
+	];
+
+	const socialMediaItems = [
+		{
+			url: "https://facebook.com",
+			icon: Facebook,
+		},
+		{
+			url: "https://linkedin.com",
+			icon: Linkedin,
+		},
+		{
+			url: "https://instagram.com",
+			icon: Instagram,
+		},
+		{
+			url: "https://instagram.com",
+			icon: Twitter,
+		},
+	];
 
 	const classes = useStyles();
 
 	return (
 		<div className={classes.insideContainer}>
-			{isLogin && <Newsletter/>}
+			{isLogin && <Newsletter />}
 			<Box className={classes.footerContainer}>
 				<Container>
 					<Box className={classes.footer}>
-						<Box className={classes.logoContainer}>
-							<Box component="img" src="images/logoSomosMas.png" alt="" />
-							<Typography variant="p" className={classes.logoText}>
-								Somos más
-							</Typography>
+						<Box>
+							<ul className={classes.footerList}>
+								{navigationItemsLeft.map(({ name, url }, index) => (
+									<li key={index}>
+										<NavLink className={classes.linkItemFooterList} to={url}>
+											{name}
+										</NavLink>
+									</li>
+								))}
+							</ul>
 						</Box>
-						<Box className={classes.itemsContainer}>
-							<Box>
-								<Typography className={classes.footerListTitle} variant="p">
-								Nuestras redes 
-								</Typography>
-								<ul className={classes.footerList}>
-									{footerData.socialMedia.map(({ name, url }, index) => (
-										<li key={index}>
-											<NavLink className={classes.linkItemFooterList} to={url}>
-												{name}
-											</NavLink>
-										</li>
-									))}
-								</ul>
-							</Box>
-							<Box>
-								<Typography className={classes.footerListTitle} variant="p">
-								Navegación
-								</Typography>
-								<ul className={classes.footerList}>
-									{footerData.navigationItems.map(({ name, url }, index) => (
-										<li key={index} >
-											<a className={classes.linkItemFooterList} href={url}>
-												{name}
-											</a>
-										</li>
-									))}
-								</ul>
-							</Box>
+						<Box component="img" src="images/logoSomosMas.png" alt="Logo somos mas" />
+						<Box>
+							<ul className={classes.footerList}>
+								{navigationItemsRight.map(({ name, url }, index) => (
+									<li key={index}>
+										<NavLink className={classes.linkItemFooterList} to={url}>
+											{name}
+										</NavLink>
+									</li>
+								))}
+							</ul>
 						</Box>
+					</Box>
+					<hr />
+					<Box className={classes.socialMediaList}>
+						<ul className={classes.footerList}>
+							{socialMediaItems.map(({ url, icon }, index) => (
+								<li className={classes.socialMediaItem} key={index}>
+									<NavLink className={classes.socialMediaLinkItem} to={url}>
+										<Box
+											className={classes.socialMediaItemIcon}
+											src={icon}
+											alt="Social Media"
+										/>
+									</NavLink>
+								</li>
+							))}
+						</ul>
 					</Box>
 				</Container>
 			</Box>
